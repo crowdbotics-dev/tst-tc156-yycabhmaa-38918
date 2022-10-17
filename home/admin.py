10e97cd544724cf62585f0aa1b51591fb0377427
Crowdbotics <engineering@crowdbotics.com>
2022-10-17 05:41:04 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Qwertg
+
+admin.site.register(Qwertg)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class Qwertg(models.Model):
+    "Generated Model"
+    qwert = models.BigIntegerField()
